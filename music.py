d7314df4c78be9427fdfd0d94fdb8a2eb7af34be
Jimmy-cs01 <jimmyjr0616@gmail.com>
2025-02-11 15:28:41 -0500

    fixed duration accuracy

--- music.py
+++ music.py
@@ -59,28 +59,33 @@ def append_to_excel(song_name, artist_name, album_name, listen_duration, excel_f
     
 def main():
     last_logged_song = None
-    listen_duration = 0
-    tick_speed = 1 # How fast the program will update in secods
+    start_time = None
     excel_file_path = input("Name of excel file (no need for .xlsx): ") + ".xlsx"
-    
+    tickSpeed = 1 
+
     while True:
         song_name, artist_name, album_name = get_current_track()
         if song_name and artist_name:
             album_name = album_name if album_name else ""
             current_song = (song_name, artist_name, album_name)
+            
             if current_song == last_logged_song:
-                listen_duration += tick_speed
+                pass  
             else:
-                if last_logged_song is not None:
+                if last_logged_song is not None and start_time is not None:
+                    listen_duration = round(time.time() - start_time)  
                     append_to_excel(last_logged_song[0], last_logged_song[1], last_logged_song[2], listen_duration, excel_file_path)
-                listen_duration = tick_speed
+                
+                start_time = time.time()
                 last_logged_song = current_song
+        
         else:
-            if last_logged_song is not None:
+            if last_logged_song is not None and start_time is not None:
+                listen_duration = round(time.time() - start_time)
                 append_to_excel(last_logged_song[0], last_logged_song[1], last_logged_song[2], listen_duration, excel_file_path)
                 last_logged_song = None
-                listen_duration = 0
+                start_time = None
         
-        time.sleep(tick_speed)
-
+        time.sleep(tickSpeed)
+        
 main()
